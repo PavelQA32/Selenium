d4ffc143166dcb779378b38adbd9b3610a1e67ba
Юлия Багуцкая <kroly2016@gmail.com>
2022-09-21 18:22:05 +0300

    happy path

---
+++ src/test/java/TestOfForm.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestOfForm {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpDriver() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void close() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void HappyPathTest(){
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий Лапочкин");
+        elements.get(1).sendKeys("+79999999999");
+        List<WebElement> tag = driver.findElements(By.tagName("span"));
+        tag.get(11).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("  gВаша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text);
+
+
+    }
+}
